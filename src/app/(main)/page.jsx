9b6486b7b8bac3f35f9ae2d3e3520c32754169d0
rishabhshunyaekai <rishabhkumar@shunyaekai.tech>
2026-04-02 11:01:18 +0530

    Fix meta description

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -13,6 +13,49 @@ import Testimonial    from "@/components/homepage/testimonial/testimonial";
 import Sustainability from "@/components/homepage/sustainability/sustainability";
 import DownloadApp    from "@/sharedComponents/downloadApp/downloadApp";
 
+export const metadata = {
+  title         : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
+  description   : "Fast EV charging in Dubai. PlusX Electric offers emergency, doorstep, and home charger installation for EV cars at home, office, or villa.",
+  keywords      : ["plusx", "plusxelectric", "plusx electric", "portable ev charger", "on demand ev charging service in dubai", "doorstep ev charging service", "fast electric chargers near me", 
+                   "ev roadside assistance service", "emergency roadside charging", "buy or renew ev insurance in minutes", "find public ev charging station", "ev charger share service"],
+  alternates    : {
+    canonical   : "/",
+  },
+  // openGraph : {
+  //   title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
+  //   description : "",
+  //   siteName    : "PlusX Electric",
+  //   locale      : "en_US",
+  //   type        : "website",
+  //   images      : [
+  //   {
+  //     url       : "/images/homepage/desktop-banner.webp",
+  //     width     : 1200,
+  //     height    : 630,
+  //     alt       : "PlusX Electric EV Charging",
+  //   },
+  // ],
+  // },
+  // twitter       : {
+  //   card        : "summary_large_image",
+  //   title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
+  //   description : "",
+  //   creator     : "https://plusxelectric.com",
+  //   images      : [image],
+  // },
+  // robots        : {
+  //   index       : true,
+  //   follow      : true,
+  //   googleBot   : {
+  //     index     : true,
+  //     follow    : true,
+  //     "max-snippet"       : -1,
+  //     "max-image-preview" : "large",
+  //     "max-video-preview" : -1,
+  //   },
+  // },
+};
+
 function HomePage() {
   return (
     <>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,46 +12,46 @@ const poppins = Poppins({
 const GTM_ID          = process.env.NEXT_PUBLIC_GTM_ID;
 export const metadata = {
   title         : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
-  description   : "Find the best recipes in the world on this website.",
-  keywords      : ["plusx", "plusxelectric", "plusx electric", "portable ev charger", "on demand ev charging service in dubai", "doorstep ev charging service", "fast electric chargers near me", 
-                   "ev roadside assistance service", "emergency roadside charging", "buy or renew ev insurance in minutes", "find public ev charging station", "ev charger share service"],
+  // description   : "Fast EV charging in Dubai. PlusX Electric offers emergency, doorstep, and home charger installation for EV cars at home, office, or villa.",
+  // keywords      : ["plusx", "plusxelectric", "plusx electric", "portable ev charger", "on demand ev charging service in dubai", "doorstep ev charging service", "fast electric chargers near me", 
+  //                  "ev roadside assistance service", "emergency roadside charging", "buy or renew ev insurance in minutes", "find public ev charging station", "ev charger share service"],
   metadataBase  : new URL(process.env.NEXT_PUBLIC_BASE_URL || "https://plusxelectric.com"),
   alternates    : {
     canonical   : "/",
   },
-  openGraph : {
-    title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
-    description : "Find the best recipes in the world on this website.",
-    siteName    : "PlusX Electric",
-    locale      : "en_US",
-    type        : "website",
-    images      : [
-    {
-      url       : "/images/homepage/desktop-banner.webp",
-      width     : 1200,
-      height    : 630,
-      alt       : "PlusX Electric EV Charging",
-    },
-  ],
-  },
-  twitter       : {
-    card        : "summary_large_image",
-    title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
-    description : "Find the best recipes in the world on this website.",
-    creator     : "https://plusxelectric.com",
-    images      : [image],
-  },
-  robots        : {
-    index       : true,
-    follow      : true,
-    googleBot   : {
-      index     : true,
-      follow    : true,
-      "max-snippet"       : -1,
-      "max-image-preview" : "large",
-      "max-video-preview" : -1,
-    },
-  },
+  // openGraph : {
+  //   title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
+  //   description : "",
+  //   siteName    : "PlusX Electric",
+  //   locale      : "en_US",
+  //   type        : "website",
+  //   images      : [
+  //   {
+  //     url       : "/images/homepage/desktop-banner.webp",
+  //     width     : 1200,
+  //     height    : 630,
+  //     alt       : "PlusX Electric EV Charging",
+  //   },
+  // ],
+  // },
+  // twitter       : {
+  //   card        : "summary_large_image",
+  //   title       : "Emergency & Doorstep EV Charging Dubai | PlusX Electric",
+  //   description : "",
+  //   creator     : "https://plusxelectric.com",
+  //   images      : [image],
+  // },
+  // robots        : {
+  //   index       : true,
+  //   follow      : true,
+  //   googleBot   : {
+  //     index     : true,
+  //     follow    : true,
+  //     "max-snippet"       : -1,
+  //     "max-image-preview" : "large",
+  //     "max-video-preview" : -1,
+  //   },
+  // },
 };
 
 function RootLayout({ children }) {
